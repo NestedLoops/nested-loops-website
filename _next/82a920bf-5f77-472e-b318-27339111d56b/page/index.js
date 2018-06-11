c10e23b2148c15d958dce78123c2ed403e676564
Kahlil Lechelt <kahlil.lechelt@logmein.com>
2018-06-11 11:56:09 +0200

    initial commit

---
+++ _next/82a920bf-5f77-472e-b318-27339111d56b/page/index.js
@@ -0,0 +1,1 @@
+module.exports=__NEXT_REGISTER_PAGE("/",function(){var e=webpackJsonp([3],{251:function(e,t,n){e.exports=n(252)},252:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:true});var r=n(4);var u=n.n(r);var a=n(75);t["default"]=function(){return u.a.createElement(a["a"],null)}}},[251]);return{page:e.default}});
