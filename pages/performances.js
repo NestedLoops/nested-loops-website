d439038a8b13558ee0265ee8ab374d02a35f9a73
Kahlil Lechelt <kahlil.lechelt@logmein.com>
2018-06-11 12:34:56 +0200

    new build

--- pages/performances.js
+++ pages/performances.js
@@ -2,13 +2,13 @@ import Page from '../layouts/page';
 
 export default () => <Page>
   <style jsx>{`
-    p {
+    div {
       max-width: 400px;
       margin: 5rem auto 0 auto;
       text-align: center;
     }
   `}</style>
-  <p>
+  <div>
     <ul>
       <li>
         <a href="http://youtu.be/e6wrCr7bzSg">2018</a>
@@ -20,5 +20,5 @@ export default () => <Page>
         <a href="http://youtu.be/lJ1kY-CSpBk">2015</a>
       </li>
     </ul>
-  </p>
+  </div>
 </Page>
