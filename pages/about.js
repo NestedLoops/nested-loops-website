1ea04cfd8003e2ef37915bd391838810bb01504f
Kahlil Lechelt <kahlil.lechelt@logmein.com>
2018-06-11 13:51:55 +0200

    new build

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,7 @@ export default () => <Page>
     }
   `}</style>
   <p>
-    We are a band that combines, video, audio and live vocals with JavaScript inside of an Electron window. 
+    We are a band that combines video, audio and live vocals with JavaScript inside of an Electron window. 
     We performed at the JSConf EU opening performances in 2015, 2017 and 2018.
   </p>
 </Page>
