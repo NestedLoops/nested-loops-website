f67e9738df896b4e826676a0311c40a1a7cfa163
Kahlil Lechelt <kahlil.lechelt@logmein.com>
2018-06-11 13:50:34 +0200

    new build

--- layouts/page.js
+++ layouts/page.js
@@ -6,6 +6,7 @@ const { Fragment } = React;
 
 export default ({ children }) => <Fragment>
   <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:300,400" rel="stylesheet" />
   </Head>
   <main>
